50758bf9f297d766585c69121ffcea786fb3ad82
codekoala <none@none>
2010-04-06 12:40:30 -0500

    Removed an old URLconf.  It was broken anyway

--- articles/urls.py
+++ articles/urls.py
@@ -23,8 +23,6 @@ urlpatterns += patterns('',
     url(r'^tag/(?P<tag>.*)/page/(?P<page>\d+)/$', views.display_blog_page, name='articles_display_tag_page'),
     url(r'^tag/(?P<tag>.*)/$', views.display_blog_page, name='articles_display_tag'),
 
-    url(r'^category/(?P<tag>.*)/$', 'django.views.generic.simple.redirect_to', {'url': '../../tag/%(tag)s'}),
-
     url(r'^author/(?P<username>.*)/page/(?P<page>\d+)/$', views.display_blog_page, name='articles_by_author_page'),
     url(r'^author/(?P<username>.*)/$', views.display_blog_page, name='articles_by_author'),
 
